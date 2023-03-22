52ce7b28149da0735ff82a7e881e64f7f8441543
VictorMML <126518881+VictorMML90@users.noreply.github.com>
2023-03-22 19:40:07 +0000

    go

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,9 +1,64 @@
 ﻿
  class Musico
   {
-        public string Nombre 
-    }
-internal class Program
+        public string Nombre  
+          {
+            get;
+            set;
+          }
+
+          public Musico(string n )
+          {
+               Nombre= n;
+          }
+          public void saluda()
+          {
+            Console.WriteLine("Hola");
+
+          }
+          public virtual void Tocar()
+          {
+            Console.WriteLine($"{Nombre} esta tocando su instrumento"); //tocando su instrumento
+          }
+
+          class baterista: Musico
+          {
+                public string bateria
+            {
+                get;
+                set;
+            }
+            public baterista(string n , string b): base(n)
+            {
+                bateria= b;
+            }
+            public override void Tocar ()
+            {
+                Console.WriteLine($"{Nombre} esta tocando su {bateria}");
+            }
+                  //posee una bateria
+                  //Tocar(); nombre toca su bateria
+          }
+          class bajista: Musico
+          {
+            public string bajo
+            {
+                get;
+                set;
+            }
+            public bajista(string n , string b): base(n)
+            {
+                bajo= b;
+            }
+            public override void Tocar ()
+            {
+                Console.WriteLine($"{Nombre} esta tocando su {bajo}");
+            }
+            //posee una bajo
+                  //Tocar(); nombre toca su bajo
+          }
+  }
+        internal class Program
     {
    
          private static void Main(string[] args)
