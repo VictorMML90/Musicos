8cb7041d9a9d2cafee918572b5afe1a3f4793f6d
VictorMML <126518881+VictorMML90@users.noreply.github.com>
2023-03-22 19:20:31 +0000

    go

--- Musicos/Program.cs
+++ Musicos/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+ class Musico
+  {
+        public string Nombre 
+    }
+internal class Program
+    {
+   
+         private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
